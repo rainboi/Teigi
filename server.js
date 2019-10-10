2702c3cc54636ccbe44504bf2ae9bc7a5bb780fc
Max Erebus <max.erebus@gmail.com>
2019-10-11 01:11:45 +0400

    added GTrans dictionary and method

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express"),
     sqlite = require("sqlite"),
     bodyParser = require("body-parser"),
+    fs = require('fs'),
     func = require("./scripts/functions.js"),
     app = express(),
-    dbPromise = sqlite.open("CambridgeDict.db");
+    cambridge = sqlite.open("./Databases/Cambridge.db"),
+    rawdata = fs.readFileSync("./Databases/GTrans.json"),
+    gtrans = JSON.parse(rawdata);
 
 
 app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
@@ -15,10 +18,12 @@ app.get("/", function (req, res) {
 })
 
 app.post("/result", async function (req, res) {
+    var dictionary = req.body.dictionary;
     var text = req.body.text
     var rawWords = func.parse(text);
     var onlyWords = func.fullParse(text);
-    dict = await getDefinitions(onlyWords, rawWords)
+    console.log(dictionary);
+    dict = await getDefinitions(onlyWords, rawWords, dictionary)
     res.render("result", { dict: dict });
 })
 
@@ -26,18 +31,41 @@ app.listen(process.env.PORT || 3000, function () {
     console.log("PORT: 3000");
 })
 
-async function getDefinitions(words, rawWords) {
+async function getDefinitions(words, rawWords, dict) {
     var dictionary = [];
-    db = await dbPromise;
-    for (let i = 0; i < words.length; i++) {
-        var result = await db.get("SELECT word, definition FROM Dictionary WHERE word = ? ", words[i]);
-        if (result) {
-            dictionary.push({ word: rawWords[i], definition: result.definition });
-        }
-        else {
-            dictionary.push({ word: rawWords[i], definition: false })
-        }
+    switch (dict) {
+        case "Cambridge":
+            {
+                db = await cambridge;
+                for (let i = 0; i < words.length; i++) {
+                    var result = await db.get("SELECT word, definition FROM Dictionary WHERE word = ? ", words[i]);
+                    if (result) {
+                        dictionary.push({ word: rawWords[i], definition: result.definition });
+                    }
+                    else {
+                        dictionary.push({ word: rawWords[i], definition: false })
+                    }
+                }
+            }
+
+            break;
+
+        case "GTrans":
+            {
+                console.log("HERE");
+                console.log(gtrans["dog"])
+                words.forEach(function (word, i) {
+                    if (gtrans[word]) {
+                        dictionary.push({ word: rawWords[i], definition: gtrans[word] });
+                    }
+                    else {
+                        dictionary.push({ word: rawWords[i], definition: false })
+                    }
+                })
+            }
+            break;
     }
+
 
     return dictionary;
 }
